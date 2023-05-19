e85dca810fdefb4da93e2182e18d6f955c491ef3
ProximaXD <panmieciu762@gmail.com>
2023-05-19 23:24:00 +0200

    script pulled

--- script.cpp
+++ script.cpp
@@ -4,47 +4,47 @@ using namespace std;
 
 int main()
 {
-    srand((unsigned)time(0));
-    int tab[20];
-
-    for (int i = 0; i < 20; i++) {
+    srand((unsigned)time(0));       //define random range 
+    int tab[20];                    //define array (tab) with 20 index
+        
+    for (int i = 0; i < 20; i++) {  //define every index from tab 
         tab[i] = 5 + rand() % 150;
     }
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 20; i++) {  //write numbers from every tab index 
         cout << " " << tab[i];
     }
     cout << endl;
 
 
-    int max = 20;
-    int tempNum;
-    int right;
-    int left;
-    right = max - 1;
+    int max = 20;                   //array size
+    int tempNum;                    //temporary variable with number
+    int right;                      //number from right side
+    int left;                       //number from left side
+    right = max - 1;                //right index is 19
 
 
-    while (right != 0) {
-        left = 0;
-        while (left != right) {
-            if (tab[left] > tab[right]) {
+    while (right != 0) {            //while right (19, 18, ..., 0)
+        left = 0;                       
+        while (left != right) {                //while left (1, 2, ... , -> n <- ... 18, 19) right
+            if (tab[left] > tab[right]) {           //exchange left number with right number if left number will be like right number
 
-                tempNum = tab[left];
+                tempNum = tab[left];            
                 tab[left] = tab[right];
                 tab[right] = tempNum;
 
             }
             else
             {
-                left++;
+                left++;                         //left -->;
             }
         }
-        right--;
+        right--;                                // <-- right
     }
 
 
 
 
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 20; i++) {      //write sorted array
         cout << " " << tab[i];
     }
 }
